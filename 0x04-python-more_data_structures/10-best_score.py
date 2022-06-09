be52a1c22d64b188c83473f6e0b8adab6049752f
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-06-09 03:02:15 +0100

    return best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.9
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     items = list(a_dictionary.items())
     items = [(i[1], i[0]) for i in items]
@@ -9,11 +9,3 @@ def best_score(a_dictionary):
     items = [(i[0], i[1]) for i in items]
     new_dict = items[-1][1]
     return new_dict
-
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
