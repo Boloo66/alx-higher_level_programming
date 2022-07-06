1b7e76ab2d7f10969bbe075e986b01c1543cabbd
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-06 21:33:05 +0100

    Python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,6 @@ def inherits_from(obj, a_class):
     obj: python object to be checked.
     a_class: the class name
     """
-    return (issubclass(obj, a_class))
+    if (issubclass(type(obj), a_class) and type(obj) != a_class):
+        return True
+    return False
