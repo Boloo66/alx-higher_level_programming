3dd9d6cecd0e2dce226c83a4a50a6e9bd7f4091a
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-06 02:27:17 +0100

    I/O in python

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module writes to a file from JSON rep"""
+import json
+
+
+def load_from_json_file(filename):
+    """This fuction write an python obj from a json file.
+    Args:
+    filename: name of th file(.json)
+    """
+    with open(filename) as f_obj:
+        json.load(f_obj)
