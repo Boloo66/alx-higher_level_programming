02ee6bbc89f08dcf9e57a899336ff25e63c77ce4
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-06 21:03:53 +0100

    Python inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,18 @@ class MyList(list):
 
     def print_sorted(self):
         """Function that prints list in sorted order"""
-        return (super().sort())
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    MyList = __import__('1-my_list').MyList
+
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
