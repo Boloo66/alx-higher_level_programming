03bd4c1d11d90148ae82c35107ea1619805511a9
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-06 22:51:59 +0100

    Python inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module template for Square that inherits from Rectangle"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class square for making square objects that inherits attribute functions from Rectangle"""
+
+    def __init__(self, size):
+        """cONSTRUCTOR METHOD FOR SUARE
+        Args:
+        size(int): size of the square.
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+
+s = Square(13)
+
+print(s)
+print(s.area())
