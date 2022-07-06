a075224d1a28e8bfc4baf8b0117b88a8d0eb076f
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-06 01:15:16 +0100

    I/O in python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,15 +7,5 @@ def read_file(filename=""):
     Args:
     @filename: name of the file to be parsed
     """
-    if filename == "":
-        raise NameError("Filename cannot be empty")
-
-    try:
-        with open(filename, encoding="utf8") as f_obj:
-            buffer = f_obj.read()
-
-    except (FileNotFoundError, NameError):
-        raise FileNotFoundError("{} not found".format(filename))
-
-    else:
-        print(buffer, end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
