87b78d8a71742feac9337b9a3dddc23b5bfa51eb
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-06 21:49:26 +0100

    Python inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -8,7 +8,3 @@ class BaseGeometry:
     def area(self):
         """Empty initializer for class"""
         raise Exception("area() is not implemented")
-
-    def __getattribute__(self, __name: str) -> None:
-        """Magic attribute method"""
-        raise Exception("area() is not implemented")
