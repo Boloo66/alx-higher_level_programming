3fd5d4ce22fa1ea677a4da44f8cb3dc44cadc1e0
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-06 22:17:45 +0100

    Python inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""The module for class Rectangle"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Initialize a new Rectangle class that inherits fron BaseGeometry"""
+
+    def __init__(self, width: int, height: int) -> None:
+        """Constructor for the Rectangle Class.
+        Args:
+        width: integer for rectangle width.
+        height: integer for rectangle height.
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
+
+try:
+    print("Rectangle: {} - {}".format(r.width, r.height))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    r2 = Rectangle(4, True)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
