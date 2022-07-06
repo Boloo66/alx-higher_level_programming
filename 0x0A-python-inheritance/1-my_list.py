4c66e30bba0b678b5191a404168d29525d96f2f3
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-07-06 20:59:23 +0100

    Python inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that inherites from python list class"""
+
+
+class MyList(list):
+    """MyList class that inherits from list and print list object in sorted order"""
+
+    def print_sorted(self):
+        """Function that prints list in sorted order"""
+        return (super().sort())
