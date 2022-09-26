6df02bd4a43cc3cbfd6970fbef4cd05ba5c2aeb9
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-26 15:04:07 +0100

    networking with python

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    use urllib to send post request
+    attaches the mail to the url address
+    displays the content
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+mail = sys.argv[2]
+url = sys.argv[1]
+
+value = {'email':mail}
+data = urllib.parse.urlencode(value)
+data = data.encode('ascii')
+res = urllib.request.Request(url, data)
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen(res) as e_content:
+        content = e_content.read()
+        print(content.decode('utf-8'))
