bac44388c11565fe946dc5a253da9bb4dbbd46ef
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-26 15:24:54 +0100

    networking with python

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,13 +3,14 @@
     use urllib to request a page
     traps an exception and print the error code
 """
-import urllib.request
-import urllib.error
-import sys
-
-url = sys.argv[1]
 
 if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+
+    url = sys.argv[1]
+
     try:
         with urllib.request.urlopen(url) as res:
             content = res.read()
