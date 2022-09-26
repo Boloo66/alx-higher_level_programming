d79a08841efdff8829d3d39931541257d87bb362
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-26 15:17:57 +0100

    networking with python

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    use urllib to request a page
+    traps an exception and print the error code
+"""
+import urllib.request
+import urllib.error
+import sys
+
+url = sys.argv[1]
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(url) as res:
+            content = res.read()
+            print(content.decode('utf8'))
+    except urllib.error.URLError as e:
+        if hasattr(e, 'code'):
+            print(e.code)
+        else:
+            pass
