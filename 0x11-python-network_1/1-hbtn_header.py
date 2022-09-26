14cbc36cb64f19cd0ad3ada171fd3128fdd1f2cf
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-26 14:46:38 +0100

    networking with python

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 """
-    
+    use urllib
+    - get the X-Request-Id
 """
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        content = res.read()
+
+    print(res.headers['X-Request-Id'])
