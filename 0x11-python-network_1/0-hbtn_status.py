0742216e4c48b78c9d6e28f87091d391e7c5350b
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-26 14:24:10 +0100

    network1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    use the urllib
+    request a content using urlopen
+    inspect content for:
+        type
+        content
+        utf8-content
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res1:
+        content = res1.read()
+
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode('utf8')))
