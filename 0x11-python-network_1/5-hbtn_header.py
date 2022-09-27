5d0a9b33c67e1bdf83546b70429050cf5db779fe
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-27 15:59:32 +0100

    request ID

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,4 +12,4 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with requests.get(url) as res:
-        print(res.headers["X-Request-Id"])
+        print(res.headers.get("X-Request-Id"))
