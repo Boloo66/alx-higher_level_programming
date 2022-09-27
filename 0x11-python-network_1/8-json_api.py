d8537772915fb3ba1ab5bbbc61ce18941131f5e5
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-27 16:52:53 +0100

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,13 +10,12 @@ if __name__ == "__main__":
     content = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {'q': content}
 
-    with requests.post("http://0.0.0.0:5000/search_user", data=payload) as res:
+    with requests.post("http://0.0.0.0:5000/search_user", data=payload) as r:
         try:
-            response = res.json()
+            response = r.json()
             if response == {}:
                 print("No result")
             else:
                 print("[{}] {}".format(response.get('id'), response.get('name')))
-
         except ValueError:
             print("Not a valid JSON")
