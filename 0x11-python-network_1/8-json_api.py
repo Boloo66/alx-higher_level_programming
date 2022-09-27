744a5c0d891dd8e53cc5b53dd8e0158a6ea803b4
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-27 16:42:00 +0100

    json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    send a mail request using post
+    - return the id and name
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    content = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {'q': content}
+
+    with requests.post("http://0.0.0.0:5000/search_user", data=payload) as res:
+        try:
+            response = res.json()
+            if response == {}:
+                print("No result")
+            else:
+                print({[]} {}.format(response.get('id'), response.get('name')))
+
+        except ValueError:
+            print("Not a valid JSON")
