1fb1797dd78ee738bdc26bd655d6b157730b5f75
Boloo66 <oguntadeemmanuel66@icloud.com>
2022-09-27 15:56:09 +0100

    request ID

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+    using request to get
+    - content type
+    - and OK status code from a URL
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with requests.get(url) as res:
+        print(res.headers["X-Request-Id"])
